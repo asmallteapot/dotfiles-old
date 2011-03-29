c7db4c00c2a54687afe41517a922eacd69c3489a
Bill Williams <bill@flpatriot.com>
2011-03-29 10:29:00 -0400

    added dotjs script for displaying xkcd alt–text below the comic

---
+++ js/xkcd.com.js
@@ -0,0 +1,9 @@
+// display the alt–text below the comic
+var comic = $("div.s img[title]");
+var titleP = $("<p></p>");
+titleP.css({"font-variant": "normal",
+            "border": "1px solid yellow",
+            "padding": "1em",
+            "background-color": "#FFFFCC"});
+titleP.text(comic.attr("title"));
+comic.after(titleP);
